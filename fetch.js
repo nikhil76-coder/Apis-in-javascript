24c285f7c87fe7633fda6aa2328a5b4ce228cfea
amankumar-13 <82812338+amankumar-13@users.noreply.github.com>
2021-05-12 16:21:04 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,13 +1,13 @@
-const url = "https://api.wheretheiss.at/v1/satellites/25544"; 
+const url = "https://api.zippopotam.us/us/33162"; 
 
 async function getData() { 
     const response = await fetch(url);
 
     const data = await response.json();
     console.log(data);
-    const { latitude, longitude } = data;
-    document.getElementById("lat").textContent = latitude;
-    document.getElementById("long").textContent = longitude;
+    const { country , places } = data;
+    document.getElementById("con").textContent = country;
+    document.getElementById("pla").textContent = places;
 
 
 
